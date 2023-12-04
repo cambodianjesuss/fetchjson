df6220de76f7270fa44c83e8bb6ceb95d8b1e98c
Boi <cambodianjesuss@gmail.com>
2023-12-03 22:55:45 -0800

    update

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -10,3 +10,27 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Arrays
+
+// Array of strings
+let colors: string[] = ['red', 'green', 'blue', ]
+// Array of numbers
+let myNumbers: number[] = [1,3,5,7]
+// Array of booleans
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {
+
+}
+let car: Car = new Car()
+
+// Object literal
+let point: {x: number; y: number} = {
+  x: 10,
+  y: 20,
+}
+
+
+
