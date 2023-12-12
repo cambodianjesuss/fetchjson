1aa8263a599685457fb7b863008839744943c78c
Boi <cambodianjesuss@gmail.com>
2023-12-11 18:07:05 -0800

    notes

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -4,6 +4,8 @@ const add = (a: number, b: number): boolean => {
   return true;
 };
 
+// Annotate the arguments, then the retun type, then arrow
+
 const sub = (x: number, y: number): number => {
   return x - y;
 }
