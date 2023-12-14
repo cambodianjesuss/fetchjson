8f76d2e73d55901f25319161988805cd4a297e00
Boi <cambodianjesuss@gmail.com>
2023-12-13 22:47:03 -0800

    test

--- annotations/objects.ts
+++ annotations/objects.ts
@@ -3,3 +3,18 @@ interface Person {
   name: string;
   age: number;
 }
+
+
+const user = {
+  firstName: '',
+  dob: 30,
+  location: {
+    city: 'Novato',
+    stage: 'CA'
+  },
+  greet(){
+    
+  }
+}
+
+const {firstName, dob}: {dob: number, firstName: string} = user;
